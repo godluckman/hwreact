ecdea328407fe967a3a11d72bea974a579fec8a5
godluckman <73855251+godluckman@users.noreply.github.com>
2021-11-23 10:13:50 +0300

    preFinal

--- Homework/CalendarHW/script.js
+++ Homework/CalendarHW/script.js
@@ -12,14 +12,12 @@ const gotSelectMonth = document.querySelector("#select-month");
 
 
 function selectCreate(elem, arr) {
-    let select = elem;
-    let options = arr;
-    for (let i = 0; i < options.length; i++) {
-        let opt = options[i];
+    for (let i = 0; i < arr.length; i++) {
+        let opt = arr[i];
         let nElem = document.createElement("option");
         nElem.textContent = opt;
         nElem.value = opt;
-        select.appendChild(nElem);
+        elem.appendChild(nElem);
     }
 }
 
@@ -40,7 +38,6 @@ const dataSet = () => {
 gotSelectMonth.addEventListener("change", dataSet);
 gotSelectYear.addEventListener("change", dataSet);
 
-
 const createButton = document.querySelector('.createbutton');
 const deleteButton = document.querySelector('.deletebutton');
 const container = document.querySelector('.container');
@@ -48,8 +45,6 @@ const container = document.querySelector('.container');
 createButton.addEventListener('click', function() {
     document.querySelector('.deletebutton').disabled = false
     calendars.push(new Calendar(date, count++));
-    //createTemplate();
-    //renderCalendar();
     console.log(calendars);
 });
 
@@ -61,6 +56,7 @@ deleteButton.addEventListener('click', function() {
         document.querySelector('.deletebutton').disabled = true;
     }
 });
+
 
 
 
@@ -123,15 +119,10 @@ class Calendar {
     }
 
     renderCalendar() {
-
         this.date.setDate(1);
-
         const monthDays = document.querySelector(`.days${this.num}`);
-
         const lastDay = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate();
-
         const prevLastDay = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
-
         const firstDayIndex = this.date.getDay() - 1;
 
         const lastDayIndex = new Date(
@@ -149,7 +140,7 @@ class Calendar {
         let days = '';
 
         for (let x = firstDayIndex; x > 0; x--) {
-            days += `<div class='prev-date${this.num}'>${prevLastDay - x + 1}</div>`;
+            days += `<div class='prev-date prev-date${this.num}'>${prevLastDay - x + 1}</div>`;
         }
 
         for (let i = 1; i <= lastDay; i++) {
@@ -164,118 +155,8 @@ class Calendar {
         }
 
         for (let j = 1; j <= nextDays + 1; j++) {
-            days += `<div class='next-date${this.num}'>${j}</div>`;
+            days += `<div class='next-date next-date${this.num}'>${j}</div>`;
             monthDays.innerHTML = days;
         }
-
     }
-
-
-
 }
-
-
-
-
-
-
-// const createTemplate = () => {
-//     const container = document.querySelector('.container') // '.container${this.num}'
-//     let sample = `<div class="calendar">
-//                 <div class="month">
-//                     <i class="fas fa-angle-double-left prev-year"></i>
-//                     <i class="fas fa-angle-left prev"></i>
-//                     <div class="date">
-//                         <h1></h1>
-//                         <p></p>
-//                     </div>
-//                     <i class="fas fa-angle-right next"></i>
-//                     <i class="fas fa-angle-double-right next-year"></i>
-//                 </div>
-//                 <div class="weekdays">
-//                     <div>Пн</div>
-//                     <div>Вт</div>
-//                     <div>Ср</div>
-//                     <div>Чт</div>
-//                     <div>Пт</div>
-//                     <div>Сб</div>
-//                     <div>Вс</div>
-//                 </div>
-//                 <div class="days"></div>
-//                 </div>`;
-//     container.insertAdjacentHTML("beforeend", sample);
-
-//     document.querySelector('.prev').addEventListener('click', () => {
-//         date.setMonth(date.getMonth() - 1);
-//         renderCalendar();
-//     });
-
-//     document.querySelector('.next').addEventListener('click', () => {
-//         date.setMonth(date.getMonth() + 1);
-//         renderCalendar();
-//     });
-
-//     document.querySelector('.prev-year').addEventListener('click', () => {
-//         date.setFullYear(date.getFullYear() - 1);
-//         renderCalendar();
-//     });
-
-//     document.querySelector('.next-year').addEventListener('click', () => {
-//         date.setFullYear(date.getFullYear() + 1);
-//         renderCalendar();
-//     });
-// }
-
-
-// const renderCalendar = () => {
-//     date.setDate(1);
-
-//     const monthDays = document.querySelector('.days');
-
-//     const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-
-//     const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-
-//     const firstDayIndex = date.getDay() - 1;
-
-//     const lastDayIndex = new Date(
-//         date.getFullYear(),
-//         date.getMonth() + 1,
-//         0
-//     ).getDay();
-
-//     const nextDays = 7 - lastDayIndex - 1;
-//     const months = montharr.slice(1);
-
-
-//     document.querySelector('.date h1').innerHTML = months[date.getMonth()];
-
-//     document.querySelector('.date p').innerHTML = date.getFullYear() + ' год';
-
-//     let days = '';
-
-//     for (let x = firstDayIndex; x > 0; x--) {
-//         days += `<div class='prev-date'>${prevLastDay - x + 1}</div>`;
-//     }
-
-//     for (let i = 1; i <= lastDay; i++) {
-//         // if (
-//         //     i === new Date().getDate() &&
-//         //     date.getMonth() === new Date().getMonth()
-//         // ) {
-//         //     days += `<div class='today'>${i}</div>`;
-//         // } else {
-//         days += `<div>${i}</div>`;
-//         // }
-//     }
-
-//     for (let j = 1; j <= nextDays + 1; j++) {
-//         days += `<div class='next-date'>${j}</div>`;
-//         monthDays.innerHTML = days;
-//     }
-
-
-// };
-
-
-////////////////////////////////////////////////////////////////////////////
