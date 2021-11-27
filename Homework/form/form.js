efc88c6e528cde5e830f31222a2db8f87e9a0b08
godluckmaan <zheka20000103@gmail.com>
2021-11-27 07:44:22 +0300

    Fix

--- Homework/form/form.js
+++ Homework/form/form.js
@@ -1,46 +1,32 @@
-let attempt = 3;
-while (attempt !== 0) {
-    const surname = prompt('Укажите свою фамилию:');
-    const firstName = prompt('Укажите свою имя:');
-    const patronymic = prompt('Укажите свою отчество:');
-    const age = prompt('Укажите количество полных лет:');
-    let gender = confirm("Ваш пол - мужской?");
-
-    if (age < 99 && age > 0 &&
-        firstName.length < 20 && firstName.length > 0 &&
-        surname.length < 20 && surname.length > 0 &&
-        patronymic.length < 20 && patronymic.length > 0 &&
-        typeof surname === 'string' && typeof patronymic === 'string' && typeof firstName === 'string') {
-        let old, oldEnough;
-
-        if (gender) {
-            gender = "мужской";
-        } else {
-            gender = "женский";
-        }
-
-        if (age >= 60) {
-            old = 'да'
-        } else {
-            old = 'нет'
-        }
-
-        if (age > 18) {
-            oldEnough = 'Вы уже совершеннолетний'
-        } else {
-            oldEnough = 'Вы не совершеннолетний'
-        }
-
-        alert('Ваше ФИО: ' + surname + ' ' + firstName + ' ' + patronymic + '\n' +
-            'Ваш возраст в годах: ' + age + '\n' +
-            'Ваш возраст в днях: ' + age * 365 + '\n' +
-            oldEnough + '\n' +
-            'Ваш пол: ' + gender + '\n' +
-            'Вы на пенсии: ' + old
-        );
-    } else {
-        alert("Error. Invalid value");
-    }
-    attempt--;
+const userInput = () => {
+    const surname = inputString('Укажите свою фамилию:', checkString);
+    const name = inputString('Укажите свою имя:', checkString);
+    const patronymic = inputString('Укажите свою отчество:', checkString);
+    const age = parseInt(inputString('Укажите количество полных лет:', checkNumber));
+    const gender = confirm("Ваш пол - мужской?") ? "мужской" : "женский";
+    return { surname, name, patronymic, age, gender };
 }
-alert('Превышено количество попыток, обновите страницу.');
+
+const printUserData = (userData) => {
+    alert(`Ваше ФИО: ${userData.surname} ${userData.name} ${userData.patronymic}
+Ваш возраст в годах: ${userData.age}
+Ваш возраст в днях: ${userData.age*365}
+${userData.age >= 18 ? "Вы уже совершеннолетний" : "Вы не совершеннолетний"}
+Ваш пол: ${userData.gender}
+Вы на пенсии: ${userData.age>=60 ? "да" : "нет"}`)
+}
+
+const cyrillicPattern = /^\p{Script=Cyrillic}+$/u;
+const checkNumber = (str) => str < 99 && str > 6;
+const checkString = (str) => cyrillicPattern.test(str);
+
+const inputString = (msg, check) => {
+    while (true) {
+        const str = prompt(msg);
+        if (check(str))
+            return str;
+    }
+}
+
+const userData = userInput();
+printUserData(userData);
