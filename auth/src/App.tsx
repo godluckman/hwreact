198d109b1a5b4d5f0ea1c8d2ae59b4809f70c0da
godluckman <73855251+godluckman@users.noreply.github.com>
2021-12-28 15:14:37 +0300

    List1

--- auth/src/App.tsx
+++ auth/src/App.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" value="John"/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
